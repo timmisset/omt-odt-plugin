e89a9196e9478b2a2493fc633ad00594087bad74
timmisset <timmisset@live.com>
2022-03-13 08:14:04 +0100

    null check

--- src/test/java/com/misset/opp/settings/SettingsStateTest.java
+++ src/test/java/com/misset/opp/settings/SettingsStateTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SettingsStateTest {
     SettingsState settingsState;
@@ -19,28 +18,33 @@ class SettingsStateTest {
     }
 
     @Test
-    void getMappingPath() {
+    void testGetMappingPath() {
         String mappingPath = settingsState.getMappingPath("@domainA/fileA.omt");
         mappingPath = mappingPath.replaceAll("\\\\", "/");
         assertTrue(mappingPath.endsWith("someLocation/domain/A/fileA.omt"));
     }
 
     @Test
-    void getMappingPathSubfolder() {
+    void testGetMappingPathSubfolder() {
         String mappingPath = settingsState.getMappingPath("@domainA/subfolder/fileA.omt");
         mappingPath = mappingPath.replaceAll("\\\\", "/");
         assertTrue(mappingPath.endsWith("someLocation/domain/A/subfolder/fileA.omt"));
     }
 
     @Test
-    void getShorthandPathRoot() {
+    void testGetShorthandPathRoot() {
         String fullPath = Path.of("src/test/resources/someLocation/domain/A/fileA.omt").toAbsolutePath().toString();
         assertEquals(Path.of("@domainA/fileA.omt").toString(), settingsState.getShorthandPath(fullPath));
     }
 
     @Test
-    void getShorthandPathSubfolder() {
+    void testGetShorthandPathSubfolder() {
         String fullPath = Path.of("src/test/resources/someLocation/domain/B/another/folder/or/subfolder/fileB.omt").toAbsolutePath().toString();
         assertEquals(Path.of("@domainB/another/folder/or/subfolder/fileB.omt").toString(), settingsState.getShorthandPath(fullPath));
+    }
+
+    @Test
+    void testGetMappingPathReturnsNullWhenNotFound() {
+        assertNull(settingsState.getMappingPath("@domainC/subfolder/fileA.omt"));
     }
 }
