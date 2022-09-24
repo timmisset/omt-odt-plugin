84b23cb5b9cb79d430305354fe5099d43a7b6933
timmisset <timmisset@live.com>
2022-09-24 06:15:11 +0200

    Added #185

--- src/main/java/com/misset/opp/odt/inspection/calls/operators/ODTOperatorInspectionIIf.java
+++ src/main/java/com/misset/opp/odt/inspection/calls/operators/ODTOperatorInspectionIIf.java
@@ -5,16 +5,19 @@ import com.intellij.codeInspection.util.IntentionFamilyName;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.misset.opp.model.OntologyModel;
 import com.misset.opp.odt.ODTElementGenerator;
 import com.misset.opp.odt.builtin.operators.IIfOperator;
 import com.misset.opp.odt.psi.ODTQuery;
+import com.misset.opp.odt.psi.ODTSignatureArgument;
 import com.misset.opp.odt.psi.resolvable.call.ODTCall;
-import com.misset.opp.resolvable.Resolvable;
+import org.apache.jena.ontology.OntResource;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Code inspection for all unused declarations
@@ -44,34 +47,57 @@ public class ODTOperatorInspectionIIf extends LocalInspectionTool {
 
     private void inspectIIfOperator(@NotNull ProblemsHolder holder,
                                     @NotNull ODTCall call) {
-        if (call.getCallable() == IIfOperator.INSTANCE) {
-            if (canBeSimplified(call)) {
-                holder.registerProblem(call, UNNECESSARY_IIF, ProblemHighlightType.WARNING, getSimplifyQuickfix(call));
-            } else if (canBeCombined(call)) {
-                holder.registerProblem(call, UNNECESSARY_IIF, ProblemHighlightType.WARNING, getCombineQuickfix(call));
-            }
+        if (call.getCallable() == IIfOperator.INSTANCE && canBeSimplified(call)) {
+            holder.registerProblem(call, UNNECESSARY_IIF, ProblemHighlightType.WARNING, getSimplifyQuickfix(call));
         }
     }
 
     private boolean canBeSimplified(@NotNull ODTCall call) {
-        return call.getSignatureArguments()
-                .stream()
-                .skip(1)
-                .allMatch(argument -> argument.isPrimitiveArgument() && argument.isBoolean());
+        OntologyModel ontologyModel = OntologyModel.getInstance();
+        List<ODTSignatureArgument> signatureArguments = call.getSignatureArguments();
+        if (signatureArguments.size() == 2) {
+            return signatureArguments.get(1).isBoolean();
+        } else if (signatureArguments.size() == 3) {
+            return (ontologyModel.isBooleanTrue(signatureArguments.get(1).resolve()) &&
+                    ontologyModel.isBooleanInstance(signatureArguments.get(2).resolve())) ||
+                    (ontologyModel.isBooleanFalse(signatureArguments.get(1).resolve()) &&
+                            ontologyModel.isBooleanTrue(signatureArguments.get(2).resolve()));
+        }
+        return false;
     }
 
-    private boolean canBeCombined(@NotNull ODTCall call) {
-        return call.getNumberOfArguments() == 2 && Optional.ofNullable(call.getSignatureArgument(1))
-                .map(Resolvable::isBoolean)
-                .orElse(false);
-    }
-
-    @NotNull
+    @Nullable
     private LocalQuickFix getSimplifyQuickfix(@NotNull ODTCall call) {
-        boolean negate = "false".equals(call.getSignatureValue(1));
-        String query = String.format("%s%s", negate ? "NOT " : "", getCondition(call));
-
-        return getQuickFix(SIMPLIFY, query);
+        List<ODTSignatureArgument> signatureArguments = call.getSignatureArguments();
+        Set<OntResource> ifTrue = call.getSignatureArgument(1).resolve();
+        String condition = getCondition(call);
+        OntologyModel ontologyModel = OntologyModel.getInstance();
+        String query = null;
+        if (signatureArguments.size() == 2) {
+            if (ontologyModel.isBooleanFalse(ifTrue)) {
+                // IIF(<condition>, false) => NOT <condition>
+                query = "NOT " + condition;
+            } else if (ontologyModel.isBooleanTrue(ifTrue)) {
+                // IIF(<condition>, true) => NOT <condition>
+                query = condition;
+            } else if (ontologyModel.isBooleanInstance(ifTrue)) {
+                // IIF(<condition>, <boolean>) => <condition> AND <boolean>
+                query = condition + " AND " + call.getSignatureArgument(1).getText();
+            }
+        } else if (signatureArguments.size() == 3) {
+            Set<OntResource> ifFalse = call.getSignatureArgument(2).resolve();
+            if (ontologyModel.isBooleanTrue(ifTrue) && ontologyModel.isBooleanFalse(ifFalse)) {
+                // IIF(<condition>, true, false) => <condition>
+                query = condition;
+            } else if (ontologyModel.isBooleanTrue(ifFalse) && ontologyModel.isBooleanFalse(ifTrue)) {
+                // IIF(<condition>, false, true) => NOT <condition>
+                query = "NOT " + condition;
+            } else if (ontologyModel.isBooleanTrue(ifTrue) && ontologyModel.isBooleanInstance(ifFalse)) {
+                // IIF(<condition>, true, <boolean>) => <condition> OR <boolean>
+                query = condition + " OR " + call.getSignatureArgument(2).getText();
+            }
+        }
+        return query != null ? getQuickFix(SIMPLIFY, query) : null;
     }
 
     @NotNull
@@ -95,10 +121,9 @@ public class ODTOperatorInspectionIIf extends LocalInspectionTool {
             public void applyFix(@NotNull Project project,
                                  @NotNull ProblemDescriptor descriptor) {
                 final ODTQuery replacement = ODTElementGenerator.getInstance(project).fromFile(query, ODTQuery.class);
-                if (replacement == null) {
-                    return;
+                if (replacement != null) {
+                    descriptor.getPsiElement().replace(replacement);
                 }
-                descriptor.getPsiElement().replace(replacement);
             }
         };
     }
--- src/test/java/com/misset/opp/odt/inspection/calls/operators/ODTOperatorInspectionIIfTest.java
+++ src/test/java/com/misset/opp/odt/inspection/calls/operators/ODTOperatorInspectionIIfTest.java
@@ -3,11 +3,16 @@ package com.misset.opp.odt.inspection.calls.operators;
 import com.misset.opp.odt.testcase.ODTTestCase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Collections;
+import java.util.stream.Stream;
 
-import static com.misset.opp.odt.inspection.calls.operators.ODTOperatorInspectionIIf.*;
+import static com.misset.opp.odt.inspection.calls.operators.ODTOperatorInspectionIIf.SIMPLIFY;
+import static com.misset.opp.odt.inspection.calls.operators.ODTOperatorInspectionIIf.UNNECESSARY_IIF;
 
 class ODTOperatorInspectionIIfTest extends ODTTestCase {
 
@@ -17,89 +22,45 @@ class ODTOperatorInspectionIIfTest extends ODTTestCase {
         myFixture.enableInspections(Collections.singleton(ODTOperatorInspectionIIf.class));
     }
 
-    @Test
-    void testIIfOperatorShowsNoWarningWhenNoBooleanSignatureArguments() {
-        String content = "IIF($condition, 'a', 'b')";
-        configureByText(content);
-        inspection.assertNoWarning(UNNECESSARY_IIF);
+    public static Stream<Arguments> getSimplyStatements() {
+        return Stream.of(
+                Arguments.of("IIF($condition, true)", "$condition"),
+                Arguments.of("IIF($condition, false)", "NOT $condition"),
+                Arguments.of("IIF($condition, $variable == $anotherVariable)", "$condition AND $variable == $anotherVariable"),
+                Arguments.of("IIF($condition, true, false)", "$condition"),
+                Arguments.of("IIF($condition, false, true)", "NOT $condition"),
+                Arguments.of("IIF($condition, true, $variable == $anotherVariable)", "$condition OR $variable == $anotherVariable")
+        );
     }
 
-    @Test
-    void testIIfOperatorShowsNoWarningWhenOneBooleanSignatureArguments() {
-        String content = "IIF($condition, true, 'b')";
-        configureByText(content);
-        inspection.assertNoWarning(UNNECESSARY_IIF);
-    }
-
-    @Test
-    void testIIfOperatorShowsNoWarningWhenOneBooleanSignatureArgumentsOnFalse() {
-        String content = "IIF($condition, 'a', true)";
-        configureByText(content);
-        inspection.assertNoWarning(UNNECESSARY_IIF);
-    }
-
-    @Test
-    void testIIfOperatorShowsSimplifyWarning() {
-        String content = "IIF($condition, true, false)";
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "IIF($condition, true, false)",
+            "IIF($condition, true, 'a' == 'b')",
+            "IIF($condition, $variable == 'a')"
+    })
+    void testHasUnnecessaryIIFWarning(String content) {
         configureByText(content);
         inspection.assertHasWarning(UNNECESSARY_IIF);
     }
 
-    @Test
-    void testIIfOperatorShowsCombineWarning() {
-        String content = "IIF($condition, $variable == 'a')";
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "IIF($condition, 'a', 'b')",
+            "IIF($condition, true, 'b')",
+            "IIF($condition, 'a', true)"
+
+    })
+    void testHasNoUnnecessaryIIFWarning(String content) {
         configureByText(content);
-        inspection.assertHasWarning(UNNECESSARY_IIF);
+        inspection.assertNoWarning(UNNECESSARY_IIF);
     }
 
-    @Test
-    void testIIfOperatorRefactorSimplified() {
-        String content = "IIF($condition, true, false)";
+    @ParameterizedTest
+    @MethodSource(value = "getSimplyStatements")
+    void testInvokeSimplify(String content, String result) {
         configureByText(content);
-        Assertions.assertTrue(getFile().getText().contains("IIF($condition, true, false)"));
         inspection.invokeQuickFixIntention(SIMPLIFY);
-        Assertions.assertTrue(getFile().getText().contains("$condition"));
-        Assertions.assertFalse(getFile().getText().contains("IIF($condition, true, false)"));
+        Assertions.assertEquals(result, getFile().getText());
     }
-
-    @Test
-    void testIIfOperatorRefactorSimplifiedNegated() {
-        String content = "IIF($condition, false, true)";
-        configureByText(content);
-        Assertions.assertTrue(getFile().getText().contains("IIF($condition, false, true)"));
-        inspection.invokeQuickFixIntention(SIMPLIFY);
-        Assertions.assertTrue(getFile().getText().contains("NOT $condition"));
-        Assertions.assertFalse(getFile().getText().contains("IIF($condition, true, false)"));
-    }
-
-    @Test
-    void testIIfOperatorRefactorSimplifiedNoOtherwise() {
-        String content = "IIF($condition, true)";
-        configureByText(content);
-        Assertions.assertTrue(getFile().getText().contains("IIF($condition, true)"));
-        inspection.invokeQuickFixIntention(SIMPLIFY);
-        Assertions.assertTrue(getFile().getText().contains("$condition"));
-        Assertions.assertFalse(getFile().getText().contains("IIF($condition, true)"));
-    }
-
-    @Test
-    void testIIfOperatorRefactorSimplifiedNegatedNoOtherwise() {
-        String content = "IIF($condition, false)";
-        configureByText(content);
-        Assertions.assertTrue(getFile().getText().contains("IIF($condition, false)"));
-        inspection.invokeQuickFixIntention(SIMPLIFY);
-        Assertions.assertTrue(getFile().getText().contains("NOT $condition"));
-        Assertions.assertFalse(getFile().getText().contains("IIF($condition, false)"));
-    }
-
-    @Test
-    void testIIfOperatorRefactorCombine() {
-        String content = "IIF($condition, $variable == 'a')";
-        configureByText(content);
-        Assertions.assertTrue(getFile().getText().contains("IIF($condition, $variable == 'a')"));
-        inspection.invokeQuickFixIntention(COMBINE);
-        Assertions.assertTrue(getFile().getText().contains("$condition AND $variable == 'a'"));
-        Assertions.assertFalse(getFile().getText().contains("IIF($condition, $variable == 'a')"));
-    }
-
 }
